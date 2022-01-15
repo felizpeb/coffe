04ce6ce64bdc0eae8be7094c44846c39895aa505
Felipe Barreto <felizpebasan@gmail.com>
2022-01-14 22:29:14 -0300

    exercicio retangulo

---
+++ retangulo.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip> //  para utilizar o setprecision(2)
+#include <math.h> // para fazer potenciacao x²...
+
+using namespace std;
+/*
+--------------------------------------------------------------------
+exercicio
+
+fazer um programa para ler as medidas da 
+base e altura de um retângulo. Em seguida, mostrar o valor
+da área, perímetro e diagonal deste retângulo, 
+com quatro casas decimais, conforme exemplos.
+--------------------------------------------------------------------
+*/
+int main()
+{
+    float b, h, area, diagonal, perimetro;
+
+    cout << "Digite a altura (h) do retangulo:  ";
+    cin >> h;
+    cout << "Digite a base (b) do retangulo: ";
+    cin >> b;
+  
+    area = (h*b);
+    perimetro = (h*2)+(b*2);
+    diagonal = sqrt((pow(b,2))+(pow(h,2))); // pow utilizado para elevar um numero
+
+
+     cout << "\n A area do seu retangulo " << fixed << setprecision(4) << area; 
+    cout << "\n O diagonal do seu retangulo " << fixed << setprecision(4) << diagonal; 
+    cout << "\n O perimetro do seu retangulo " << fixed << setprecision(4) << perimetro << endl; 
+
+    return 0;
+}
